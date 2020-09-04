5570caf852889adf946f0a1284a7d818d2438676
SURATJYOTI DUTTA <duttasuratjyoti1999@gmail.com>
2020-09-04 12:41:00 +0530

    new file added

---
+++ src/dutta/abc.java
@@ -0,0 +1,13 @@
+package dutta;
+
+public class abc {
+
+}
+class Simple{  
+    public static void main(String args[]){  
+    	System.out.println("Hello Java");  
+    	System.out.println("Hello");
+    	System.out.println("gadha");
+    	
+    }  
+}  
